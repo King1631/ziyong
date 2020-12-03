9f194521346eaaaf903caafac021228be4c645d1
wang-max1 <61648337+wang-max1@users.noreply.github.com>
2020-12-03 22:22:24 +0800

    Update qqreadd.js

--- quan/qqreadd.js
+++ quan/qqreadd.js
@@ -113,9 +113,11 @@ tz+='👤'+'\n'
 function all()
 
  {
-   for(var i=0;i<18;i++)
+
+   for(var i=0;i<16;i++)
  { (function(i) {
             setTimeout(function() {
+
      if (i==0)
 qqreadinfo();//用户名
 
@@ -125,50 +127,45 @@ qqreadconfig();//时长查询
 else if (i==2)
 qqreadtask();//任务列表
 
-else if (i==3&&task.data.taskList[2].doneFlag==0)
+else if (i==3)
 qqreadsign();//金币签到
 
 else if (i==4&&task.data.treasureBox.doneFlag==0)
 qqreadbox();//宝箱
 
-else if (i==5&&task.data.taskList[1].doneFlag==0)
+else if (i==5&&task.data.taskList[2].doneFlag==0)
 qqreadssr1();//阅读金币1
 
-else if (i==6&&config.data.pageParams.todayReadSeconds/3600<=maxtime)
+else if (i==6)
 qqreadtime();//上传时长
 
-else if (i==7&&task.data.taskList[2].doneFlag==0)
+else if (i==7&&task.data.taskList[0].doneFlag==0)
 qqreadtake();//阅豆签到
 
-else if (i==8&&task.data.taskList[0].doneFlag==0)
+else if (i==8&&task.data.taskList[1].doneFlag==0)
 qqreaddayread();//阅读任务
 
-else if (i==9&&task.data.taskList[1].doneFlag==0)
+else if (i==9&&task.data.taskList[2].doneFlag==0)
 qqreadssr2();//阅读金币2
 
 else if (i==10&&task.data.taskList[3].doneFlag==0)
 qqreadvideo();//视频任务
 
-else if(i==11&&task.data.taskList[2].doneFlag==0)
+else if(i==11&&sign.data.videoDoneFlag==0)
 qqreadsign2();//签到翻倍
 
 else if (i==12&&task.data.treasureBox.videoDoneFlag==0)
 qqreadbox2();//宝箱翻倍
 
-else if (i==13&&task.data.taskList[1].doneFlag==0)
+else if (i==13&&task.data.taskList[2].doneFlag==0)
 qqreadssr3();//阅读金币3
 
 else if (i==14)
 qqreadwktime();//周时长查询
 
+
 else if (i==15)
 qqreadpick();//领周时长奖励
-
-else if (i==16)
-showmsg();//通知
-
-else if (i==17)
-$.已办();//结束
 
     }
 , (i + 1) *dd*1000);
