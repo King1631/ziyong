a8e5974536db78826ee0444d8196b97eef2c6fd7
wang-max1 <61648337+wang-max1@users.noreply.github.com>
2021-01-11 13:58:20 +0800

    Update qqreads.js

--- quan/qqreads.js
+++ quan/qqreads.js
@@ -56,10 +56,10 @@ hostname=mqqapi.reader.qq.com
 ############## 圈x
 
 #企鹅读书获取更新body
-https:\/\/mqqapi\.reader\.qq\.com\/log\/v4\/mqq\/track url script-request-body https://raw.githubusercontent.com/ziye12/JavaScript/master/Task/qqreads.js
+https:\/\/mqqapi\.reader\.qq\.com\/log\/v4\/mqq\/track url script-request-body https://raw.githubusercontent.com/wang-max1/-x/main/quan/qqreads.js
 
 #企鹅读书获取时长cookie
-https:\/\/mqqapi\.reader\.qq\.com\/mqq\/addReadTimeWithBid? url script-request-header https://raw.githubusercontent.com/ziye12/JavaScript/master/Task/qqreads.js
+https:\/\/mqqapi\.reader\.qq\.com\/mqq\/addReadTimeWithBid? url script-request-header https://raw.githubusercontent.com/wang-max1/-x/main/quan/qqreads.js
 
 ############## loon
 
